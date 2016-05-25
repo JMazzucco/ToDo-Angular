ab21b1dd0e6df1496438675541b6aec7848bf4b7
John Mazzucco <John.Mazzucco@gmail.com>
2016-05-25 14:52:53 -0400

    Add an array of objects called items

--- app.js
+++ app.js
@@ -2,7 +2,22 @@
 	var app = angular.module('toDo', []);
 
 	app.controller('ToDoController', function(){
-
+		this.items = tasks;
 	});
+
+
+	var tasks = [
+		{
+			title: "Wash floor",
+			description: "Wash the bedroom floor"
+		},
+		{
+			title: "Wash floor",
+			description: "Wash the bedroom floor"
+		}
+	];
+
+
+
 
 })();
