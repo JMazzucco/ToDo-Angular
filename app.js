30d0333ce1b66c0d6769308aadd5fc4b7de125a2
John Mazzucco <John.Mazzucco@gmail.com>
2016-05-27 09:35:53 -0400

    Add new items to the 'items' array

--- app.js
+++ app.js
@@ -2,13 +2,19 @@
 	var app = angular.module('toDo', []);
 
 	app.controller('ToDoController', function(){
-		// this.item = {};
+		this.item = {};
 
-		// this.newItem = {};
+		this.items =[];
 
-		// this.addItem = function() {
-		// 	this.items.push(this.newItem);
-		// };
+		this.addItem = function() {
+			this.items.push(this.item);
+			this.item.title= "";
+			this.item.description= "";
+			console.log("ya dun clicked");
+			console.log(this.items.length);
+		};
+
+		console.log(this.items);
 
 	});
 
